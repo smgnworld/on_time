d09631d45329c2c02b1073393c0ebe499e13c640
Pavlo Semehen <semegen2010@gmail.com>
2021-01-24 23:50:18 +0200

    adds rubocop and resolves offenses

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
-  belongs_to :customer, optional: :true
+  belongs_to :customer, optional: true
 
   validates :name, presence: true, uniqueness: true
 end
